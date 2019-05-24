a871de3b14100dbfb5bd3212ae2bf784228e2c6a
Arcturus <ufo54153@gmail.com>
2019-05-24 22:13:59 +0800

    Add a hot key to toggle the debug console (#64)

---
+++ Assets/Scripts/Single/InputManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using IngameDebugConsole;
+using UnityEngine;
+
+namespace Single
+{
+    public class InputManager : MonoBehaviour
+    {
+        private const KeyCode ConsoleKey = KeyCode.BackQuote;
+        public DebugLogManager debugLogManager;
+
+        private void Start()
+        {
+            DontDestroyOnLoad(gameObject);
+        }
+
+        private void FixedUpdate()
+        {
+            if (Input.GetKeyDown(ConsoleKey))
+            {
+                ToggleDebugLogConsole();
+            }
+        }
+
+        private void ToggleDebugLogConsole()
+        {
+            var active = debugLogManager.gameObject.activeSelf;
+            debugLogManager.gameObject.SetActive(!active);
+        }
+    }
+}
